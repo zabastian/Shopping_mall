f3929313ae37b1791c1ba2c3aaf5bc43c4ce6e73
dornrrl0932 <dornrrl0932@naver.com>
2025-02-03 20:37:12 +0900

    Feat: 쇼핑몰 다건 조회 API 구현
    - 전체평가 점수 조회
    - 업소 상태 조회
    - 모니터링 날짜 내림차순 조회

--- src/main/java/com/example/shopping_mall/shoppingMall/repository/ShoppingMallRepository.java
+++ src/main/java/com/example/shopping_mall/shoppingMall/repository/ShoppingMallRepository.java
@@ -2,6 +2,35 @@ package com.example.shopping_mall.shoppingMall.repository;
 
 import com.example.shopping_mall.entity.shoppingMall.ShoppingMall;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 public interface ShoppingMallRepository extends JpaRepository<ShoppingMall, Long> {
+
+    //전체 평가 쇼핑몰 리스트 조회
+    List<ShoppingMall> findAllByOverallRating(int overallRating);
+
+    //상태에 따른 쇼핑몰 리스트 조회
+    List<ShoppingMall> findAllByBusinessStatus(String businessStatus);
+
+    //상태에 따른 쇼핑몰 리스트 조회 후 리스트가 비어있으면 예외발생
+    default List<ShoppingMall> findAllByBusinessStatusOrThrowException(String businessStatus){
+
+        //쇼핑몰 리스트 조회
+        List<ShoppingMall> shoppingMallList = findAllByBusinessStatus(businessStatus);
+
+        //리스트 비어있는 경우 예외발생
+        if (shoppingMallList.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"해당 상태가 존재하지 않습니다.");
+        }
+
+        return shoppingMallList;
+    }
+
+    //모니터링 날짜를 기준으로 내림차순으로 쇼핑몰 리스트 조회
+    //OrderBy -> 정렬기준 설정(뒤에 오는 필드가 정렬기준이 됨), Desc -> 내림차순(만약 Asc을 사용했다면 오름차순으로 정렬)
+    List<ShoppingMall> findAllByOrderByMonitoringDateDesc();
+    
 }
--- src/main/java/com/example/shopping_mall/shoppingMall/service/ShoppingMallService.java
+++ src/main/java/com/example/shopping_mall/shoppingMall/service/ShoppingMallService.java
@@ -1,8 +1,14 @@
 package com.example.shopping_mall.shoppingMall.service;
 
+import com.example.shopping_mall.entity.shoppingMall.ShoppingMall;
+import com.example.shopping_mall.shoppingMall.dto.ShoppingMallDto;
 import com.example.shopping_mall.shoppingMall.repository.ShoppingMallRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +16,78 @@ public class ShoppingMallService {
 
     private final ShoppingMallRepository shoppingMallRepository;
 
+    //전체평가 조회
+    public List<ShoppingMallDto> overallRatingInquiry(int overallRating) {
+
+        //입력받은 별점 확인
+        if(0 > overallRating || overallRating > 3) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "평가 별점은 최소 0점, 최대 3점입니다.");
+        }
+
+        //ShoppingMall 조회
+        List<ShoppingMall> shoppingMallList = shoppingMallRepository.findAllByOverallRating(overallRating);
+
+        // ShoppingMall -> ShoppingMallDto로 변환
+        List<ShoppingMallDto> shoppingMallDtos = shoppingMallList.stream().map(shoppingMall ->
+                        new ShoppingMallDto(
+                                shoppingMall.getShoppingMallId(),
+                                shoppingMall.getBusinessName(),
+                                shoppingMall.getStoreName(),
+                                shoppingMall.getOverallRating(),
+                                shoppingMall.getDomainName(),
+                                shoppingMall.getPhoneNumber(),
+                                shoppingMall.getOperatorEmail(),
+                                shoppingMall.getBusinessStatus(),
+                                shoppingMall.getFirstReportDate(),
+                                shoppingMall.getMonitoringDate()))
+                .collect(Collectors.toList());
+
+        return shoppingMallDtos;
+    }
+
+    //업소 상태 조회
+    public List<ShoppingMallDto> businessStatusInquiry(String businessStatus) {
+
+        List<ShoppingMall> shoppingMallList = shoppingMallRepository.findAllByBusinessStatusOrThrowException(businessStatus);
+
+        // ShoppingMall -> ShoppingMallDto로 변환
+        List<ShoppingMallDto> shoppingMallDtos = shoppingMallList.stream().map(shoppingMall ->
+                        new ShoppingMallDto(
+                                shoppingMall.getShoppingMallId(),
+                                shoppingMall.getBusinessName(),
+                                shoppingMall.getStoreName(),
+                                shoppingMall.getOverallRating(),
+                                shoppingMall.getDomainName(),
+                                shoppingMall.getPhoneNumber(),
+                                shoppingMall.getOperatorEmail(),
+                                shoppingMall.getBusinessStatus(),
+                                shoppingMall.getFirstReportDate(),
+                                shoppingMall.getMonitoringDate()))
+                .collect(Collectors.toList());
+
+        return shoppingMallDtos;
+    }
+
+    //모니터링 날짜 내림차순 조회
+    public List<ShoppingMallDto> monitoringDateDescendingOrder() {
+
+        List<ShoppingMall> shoppingMallList = shoppingMallRepository.findAllByOrderByMonitoringDateDesc();
+
+        // ShoppingMall -> ShoppingMallDto로 변환
+        List<ShoppingMallDto> shoppingMallDtos = shoppingMallList.stream().map(shoppingMall ->
+                        new ShoppingMallDto(
+                                shoppingMall.getShoppingMallId(),
+                                shoppingMall.getBusinessName(),
+                                shoppingMall.getStoreName(),
+                                shoppingMall.getOverallRating(),
+                                shoppingMall.getDomainName(),
+                                shoppingMall.getPhoneNumber(),
+                                shoppingMall.getOperatorEmail(),
+                                shoppingMall.getBusinessStatus(),
+                                shoppingMall.getFirstReportDate(),
+                                shoppingMall.getMonitoringDate()))
+                .collect(Collectors.toList());
+
+        return shoppingMallDtos;
+    }
 }
