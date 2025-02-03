383681ce98d4d6f6500f88778c38155b5aed9ddf
dornrrl0932 <dornrrl0932@naver.com>
2025-02-03 20:35:11 +0900

    Feat: UserCreateRequestDto 유효성 검사 로직 추가

--- src/main/java/com/example/shopping_mall/auth/dto/request/UserCreateRequestDto.java
+++ src/main/java/com/example/shopping_mall/auth/dto/request/UserCreateRequestDto.java
@@ -1,5 +1,9 @@
 package com.example.shopping_mall.auth.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,7 +11,11 @@ import lombok.Getter;
 @AllArgsConstructor
 public class UserCreateRequestDto {
 
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
+    @Email(message = "올바른 이메일 형식을 입력해주세요.")
     private String email;
+
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     private String password;
 
 }
