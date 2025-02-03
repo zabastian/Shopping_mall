95c1226fb8933116142e3090641c19d8f71d15c2
dornrrl0932 <dornrrl0932@naver.com>
2025-02-03 10:34:10 +0900

    Feat: entity 생성
    - user
    - shoppingMall

---
+++ src/main/java/com/example/shopping_mall/entity/user/User.java
@@ -0,0 +1,21 @@
+package com.example.shopping_mall.entity.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String email;
+    private String password;
+}
+
