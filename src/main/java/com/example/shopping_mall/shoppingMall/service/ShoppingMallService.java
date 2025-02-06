fe542b6fdc5fa8446c1c3efacdb57fcb947ebb05
thdwnsdlf61 <il4500@naver.com>
2025-02-06 17:15:28 +0900

    refactor : 서비스 로직 dto반환 변경
    - 서비스에서 dto로 반환하는 메서드를 dto에서 반환받을 수 있도록 변경함

--- src/main/java/com/example/shopping_mall/shoppingMall/service/ShoppingMallService.java
+++ src/main/java/com/example/shopping_mall/shoppingMall/service/ShoppingMallService.java
@@ -21,65 +21,65 @@ public class ShoppingMallService {
     private final ShoppingMallQueryRepository shoppingMallQueryRepository;
 
     // 전체평가 점수 조회 + 업소상태 조회
-    public List<ShoppingMallDto> shoppingMallSummary(Integer overallRating, String businessStatus) {
+    public List<ShoppingMallDto> shoppingMallSummary(
+            Integer overallRating,
+            String businessStatus
+    ) {
 
         // 전체평점과 업소상태로 쇼핑몰 리스트 조회
         List<ShoppingMall> shoppingMallList
                 = shoppingMallRepository.findAllByOverallRatingAndBusinessStatusOrderByMonitoringDateDesc(overallRating, businessStatus);
 
-        //ShoppingMall -> ShoppingMallDto로 변환
-        List<ShoppingMallDto> shoppingMallDtos = convertToDtoList(shoppingMallList);
+        List<ShoppingMallDto> shoppingMallDtos = shoppingMallList
+                .stream()
+                .map(ShoppingMallDto::convertToDto)
+                .toList();
 
         return shoppingMallDtos;
     }
 
 
     // 전체평가 점수 조회 + 업소상태 조회(페이지네이션 적용)
-    public List<ShoppingMallDto> pageShoppingMallSummary(Pageable pageable, Integer overallRating, String businessStatus) {
+    public List<ShoppingMallDto> pageShoppingMallSummary(
+            Pageable pageable,
+            Integer overallRating,
+            String businessStatus
+    ) {
 
         //페이징 처리 된 쇼핑몰 데이터 조회
         Page<ShoppingMall> shoppingMallPage = shoppingMallRepository.findAllByOverallRatingAndBusinessStatusOrderByMonitoringDateDesc(overallRating, businessStatus, pageable);
 
-        //getContent -> 입력받은 page값에 해당하는 데이터 목록을 List형태로 변환
-        List<ShoppingMall> shoppingMalls = shoppingMallPage.getContent();
-
         //ShoppingMall -> ShoppingMallDto로 변환
-        List<ShoppingMallDto> shoppingMallDtos = convertToDtoList(shoppingMalls);
+        List<ShoppingMallDto> shoppingMallDtos = shoppingMallPage.stream()
+                .map(ShoppingMallDto::convertToDto)
+                .toList();
 
         return shoppingMallDtos;
     }
 
 
     // 전체평가 점수 조회 + 업소상태 조회(커서 기반 페이지네이션 적용)
-    public ShoppingMallCursorInquiryResponseDto ShoppingMallRatingAndStatusInquiry(Integer overallRating, String businessStatus, LocalDate cursor, int size) {
+    public ShoppingMallCursorInquiryResponseDto ShoppingMallRatingAndStatusInquiry(
+            Integer overallRating,
+            String businessStatus,
+            LocalDate cursor,
+            int size) {
 
         //QueryDSL로 데이터 조회
         List<ShoppingMall> shoppingMallList = shoppingMallQueryRepository.findShoppingMallByRatingAndStatusWithCursor(overallRating, businessStatus, cursor, size);
 
         //ShoppingMall -> ShoppingMallDto로 변환
-        List<ShoppingMallDto> shoppingMallDtos = convertToDtoList(shoppingMallList);
+//        List<ShoppingMallDto> shoppingMallDtos = convertToDtoList(shoppingMallList);
+
+        List<ShoppingMallDto> shoppingMallDtos = shoppingMallList
+                .stream()
+                .map(ShoppingMallDto::convertToDto)
+                .toList();
 
         //페이지의 마지막 모니터링 날짜
         LocalDate lastMonitoringDate = shoppingMallList.get(shoppingMallList.size() - 1).getMonitoringDate();
 
         return new ShoppingMallCursorInquiryResponseDto(shoppingMallDtos, lastMonitoringDate);
-    }
-
-
-    //ShoppingMall -> ShoppingMallDto로 변환 메서드
-    private List<ShoppingMallDto> convertToDtoList(List<ShoppingMall> shoppingMallList) {
-        return shoppingMallList.stream().map(shoppingMall -> new ShoppingMallDto(
-                        shoppingMall.getShoppingMallId(),
-                        shoppingMall.getBusinessName(),
-                        shoppingMall.getStoreName(),
-                        shoppingMall.getOverallRating(),
-                        shoppingMall.getDomainName(),
-                        shoppingMall.getPhoneNumber(),
-                        shoppingMall.getOperatorEmail(),
-                        shoppingMall.getBusinessStatus(),
-                        shoppingMall.getFirstReportDate(),
-                        shoppingMall.getMonitoringDate()))
-                .collect(Collectors.toList());
     }
 }
 
