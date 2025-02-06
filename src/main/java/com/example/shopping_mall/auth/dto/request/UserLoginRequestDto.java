e28b9bc1c627547e6a8e2d0b238cb322febf95bc
dornrrl0932 <dornrrl0932@naver.com>
2025-02-06 09:04:55 +0900

    Feat: UserLoginRequestDto에 @NoArgsConstructor 추가

--- src/main/java/com/example/shopping_mall/auth/dto/request/UserLoginRequestDto.java
+++ src/main/java/com/example/shopping_mall/auth/dto/request/UserLoginRequestDto.java
@@ -2,9 +2,11 @@ package com.example.shopping_mall.auth.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserLoginRequestDto {
 
     private String email;
